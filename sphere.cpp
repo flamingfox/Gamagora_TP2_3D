f0530af487946ee6b88eae42924e4ffd1d9e27d2
aureargo <aureargo@hotmail.fr>
2015-09-28 15:44:42 +0200

    intersection sphère mieux

--- sphere.cpp
+++ sphere.cpp
@@ -14,7 +14,22 @@ Sphere::Sphere(const vec3 &centre, float rayon):
 
 bool Sphere::intersect(const Rayon& r, float& minDist, float& maxDist) const
 {
-    vec3 op = centre - r.getOrigine();		// Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
+    vec3 diff = centre-r.getOrigine();
+
+    vec3 l = diff*r.getDirection();
+    float h2 = diff*diff-l*l;
+    if(h2> rayon*rayon)
+        return false;
+
+    float a2 = rayon*rayon-h2;   //pythagore: a2+h2 = rayon²    pa = point sur la sphère,  a = distance entre le point h qui forme un angle à 90° entre le rayon à h->centre
+
+    float diff2 = length2(diff);
+    float b2 = diff2 - h2;      //pythagore: h2+b2 = diff2
+
+    minDist = b2-a2;
+    maxDist = b2+a2;
+
+    /*vec3 op = centre - r.getOrigine();		// Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
     float t,
         b = dot(r.getDirection(),op),
         det = b * b - dot(op,op) + rayon * rayon;
@@ -28,7 +43,7 @@ bool Sphere::intersect(const Rayon& r, float& minDist, float& maxDist) const
         minDist = b + det;
     if(minDist < 0)
         return false;
-    return true;
+    return true;*/
     //return (t = b - det) >= 0 ? t : ((t = b + det) >= 0 ? t : noIntersect);
 }
 
